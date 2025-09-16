65b524b7ee40f3498db1a926f8665a1ab5b56799
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2025-09-16 16:23:16 -0300

    Update new_domain.py

--- examples/domains/new_domain.py
+++ examples/domains/new_domain.py
@@ -1,9 +1,6 @@
 import arcpy
 
-from gispy import (
-    utils,
-    domains
-)
+import domains
 
 from configparser import ConfigParser
 from os import (
@@ -20,37 +17,27 @@ config.read('config.ini')
 CURRENT_DIR = getcwd()
 
 NEW_DOMAINS = {
-    "ADM_poll_location_status": {
+    "LND_indigenous_land_classification": {
         "description": "",
         "field_type": "TEXT",
         "domain_type": "CODED",
         "values": {
-            'NOAVAIL': 'Cannot Use/Not Available',
-            'NOSTART': 'Not Started',
-            'ESCALATE': 'Requires Team Lead Escalation',
-            'NOCONT': 'Initial - Attempting Contact – Wrong Contact Information',
-            'EMAIL': 'Initial - Awaiting response - Initial Contact made by Email',
-            'VOICEMAIL': 'Initial - Awaiting response - Initial Contact made by leaving Voicemail',
-            'CORRESP': 'In Progress - In Correspondence with Contact',
-            'CONFIRM': 'In Progress - Verbal Confirmation',
-            'START': 'In Progress - Accessibility Assessment Started',
-            'COMPL': 'In Progress - Accessibility Assessment Complete',
-            'FAILED': 'In Progress - Accessibility Assessment Failed',
-            'AWAITSIG': 'ooked - Awaiting Contract signature',
-            'AWAITINV': 'Booked - Awaiting Invoice',
-            'INVOICED': 'Booked – Contract signed and Invoiced',
-            'BOOKED': 'Booked - No invoice required',
+            "LEG": "Legislated Land",
+            "TRT": "Treaty/Settlement Land",
+            "SGA": "Self-Governed Land",
+            "IOL": "Indigenous-Owned Land",
+            "TRD": "Traditional Use Area",
+            "REL": "Reserve Land",
+            "INT": "Interim Recognition",
+            "UNK": "Unknown",
         }
     },
 }
 
 if __name__ == "__main__":
-    local_gdb = utils.create_fgdb(CURRENT_DIR)
 
     PC_NAME = environ['COMPUTERNAME']
     run_from = "SERVER" if "APP" in PC_NAME else "LOCAL"
-
-    # TODO: Add to WEBGIS? web_ro?
 
     for dbs in [
         # [local_gdb, ],
@@ -58,9 +45,9 @@ if __name__ == "__main__":
         # WEBGIS features can use domains from SDEADM owner - don't need to create a domain for both SDEADM and WEBGIS
 
         [
-          config.get(run_from, "dev_rw"),
-          config.get(run_from, "dev_ro"),
-          config.get(run_from, "dev_web_ro_gdb")
+            config.get(run_from, "dev_rw"),
+            config.get(run_from, "dev_ro"),
+            config.get(run_from, "dev_web_ro_gdb")
         ],
 
         # [
