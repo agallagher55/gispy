3c2bb4612cda56ff1a7f43e09cc88d69e234b286
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2026-03-27 10:55:57 -0300

    Update __init__.py

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -1,19 +1,1 @@
-# Re-export public API so that external callers (e.g. parcelload/scripts/main.py)
-# can continue to use: from metadata import get_sde_metadata, update_metadata
-from metadata.metadata import (
-    strip_html_tags,
-    get_xml_text,
-    SDSFMetaData,
-    get_sde_metadata,
-    update_metadata,
-    get_workspace_features,
-)
 
-__all__ = [
-    "strip_html_tags",
-    "get_xml_text",
-    "SDSFMetaData",
-    "get_sde_metadata",
-    "update_metadata",
-    "get_workspace_features",
-]
