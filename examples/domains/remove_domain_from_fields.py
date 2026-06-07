51410fcf6d7beb0c1a040cad97bd32f64738cd0a
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2026-06-07 16:00:21 -0300

    Update remove_domain_from_fields.py

--- examples/domains/remove_domain_from_fields.py
+++ examples/domains/remove_domain_from_fields.py
@@ -1,7 +1,6 @@
 import arcpy
 
-import features
-import utils
+from gispy import features
 
 from configparser import ConfigParser
 
@@ -14,26 +13,27 @@ config = ConfigParser()
 config.read('config.ini')
 
 REMOVE_DOMAINS = {
-    "SDEADM.TRN_traffic_study": ["STREETNAME4", "STREETNAME5"],
+    "SDEADM.BLD_building_assetpoint": ["ADDBY", "MODBY"],
 }
-
 
 CURRENT_DIR = getcwd()
 
-
 if __name__ == "__main__":
+
     from datetime import datetime
 
     print(datetime.now())
 
     for dbs in [
-        # [utils.create_fgdb(CURRENT_DIR)],
-        # [config.get("SERVER", "dev_rw"), config.get("SERVER", "dev_ro"), config.get("SERVER", "dev_web_ro_gdb")],
         [
-            config.get("SERVER", "qa_rw"),
-            config.get("SERVER", "qa_ro"),
-            config.get("SERVER", "qa_web_ro_gdb")
+            config.get("SERVER", "dev_rw"),
+            config.get("SERVER", "dev_ro"),
         ],
+        # [
+        #     config.get("SERVER", "qa_rw"),
+        #     config.get("SERVER", "qa_ro"),
+        #     config.get("SERVER", "qa_web_ro_gdb")
+        # ],
         # [config.get("SERVER", "prod_rw"), config.get("SERVER", "prod_ro"), config.get("SERVER", "prod_web_ro_gdb")],
     ]:
 
@@ -42,21 +42,20 @@ if __name__ == "__main__":
         for db in dbs:
             print(f"\nDATABASE: {db}")
 
-            if db == local_gdb:
-
-                PC_NAME = environ['COMPUTERNAME']
-                prod_sde = config.get("SERVER", "prod_rw") if "APP" in PC_NAME else config.get("LOCAL", "prod_rw")
-
-                with arcpy.EnvManager(workspace=prod_sde):
-                    features.transfer_features(list(REMOVE_DOMAINS.keys()), local_gdb)
-
             with arcpy.EnvManager(workspace=db):
+
                 for feature in REMOVE_DOMAINS:
+
                     remove_fields = REMOVE_DOMAINS[feature]
 
-                    if db == local_gdb:
-                        feature = feature.replace("SDEADM.", "")
+                    for field_name in remove_fields:
 
-                    features.remove_domain(feature, remove_fields)
+                        print(f"\nRemoving domain from field '{field_name}' on '{feature}'...")
+
+                        arcpy.RemoveDomainFromField_management(
+                            in_table=feature,
+                            field_name=field_name
+                        )
+                        print(arcpy.GetMessages())
 
     print(datetime.now())
